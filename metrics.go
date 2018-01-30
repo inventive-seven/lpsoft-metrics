10b73d2251bf9da0a20fb0e63281d9e3bb14f6ff
mr.kakorin <mrkakorin@mr.kakorin-X751LN>
2018-01-30 18:50:23 +0300

    restr

--- metrics.go
+++ metrics.go
@@ -1,27 +1,12 @@
 package main
 
-import "fmt"
-import "akhenakh/statgo"
-
-type metrics struct{
-	Cpu *statgo.CPUStats
-	Memory *statgo.MemStats
-	Net []*statgo.NetIOStats
-	Processes *statgo.ProcessStats
-}
-
-func NewMetrics (s *statgo.Stat) *metrics{
-	m := new(metrics);
-	m.Cpu = s.CPUStats()
-	m.Memory = s.MemStats()
-	m.Net = s.NetIOStats()
-	m.Processes = s.ProcessStats()
-
-	return m
-}
+import(
+	"fmt"
+	linuxproc "github.com/c9s/goprocinfo/linux"
+)
 
 func main() {
-    m := NewMetrics(statgo.NewStat())
-    fmt.Println(m)
+    fmt.Println("hello")
+    stat, _ := linuxproc.ReadStat("/proc/stat")
+    fmt.Println(stat)
 }
-
